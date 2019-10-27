c900cd0a798bb0fbbaf2b58d6663dcf19144167c
MikeAWilliams <mumakin@gmail.com>
2019-10-27 09:03:22 -0600

    add new GetTestData method

--- parallel_max_element/main.cpp
+++ parallel_max_element/main.cpp
@@ -4,6 +4,20 @@
 #include <algorithm>
 #include <vector>
 
+std::vector<double> GetTestData(const size_t elements, const double mean, const double standardDeviation)
+{
+    std::random_device device;
+    std::mt19937 generator(device());
+    std::normal_distribution<double> distribution(mean, standardDeviation);
+
+    std::vector<double> result;
+    result.reserve(elements);
+    for(size_t i{0}; i < elements; ++i)
+    {
+        result.emplace_back(distribution(generator));
+    }
+    return result;
+}
 // TODO after making this work as is, add overloads.
 // take a comparison function
 // take a number of threads allowed
@@ -16,6 +30,6 @@ iterator ParalellMaxElement(iterator begin, iterator end)
 
 TEST_CASE("Happy path")
 {
-    const std::vector<int> input{4, 7, 3, 6};
+    const auto input {GetTestData(100'000, 500, 200)};
     REQUIRE(*std::max_element(input.cbegin(), input.cend()) == *ParalellMaxElement(input.cbegin(), input.cend()));
 }
