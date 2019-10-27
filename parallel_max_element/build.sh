6d745de1b099bd3c4081a152bbadeaa11af7b882
MikeAWilliams <mumakin@gmail.com>
2019-10-27 08:47:15 -0600

    redo cmake and build.sh

--- parallel_max_element/build.sh
+++ parallel_max_element/build.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=release "-DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake" 
-cmake --build .
-./parallel_max_element
+
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CMAKE_DIR=$SCRIPTPATH/build-dir/linux/cmake
+
+mkdir -p $CMAKE_DIR
+cd $CMAKE_DIR
+
+# find vcpkg in $PATH
+#   if vcpkg is not in $PATH, then the cmake script will download and install vcpkg
+VCPKG_ROOT="$( dirname "$( which vcpkg )" )"
+echo $VCPKG_ROOT
+
+cmake -G "Unix Makefiles" $SCRIPTPATH \
+    -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake \
+    && make && ./parallel_max_element
+
+cd $SCRIPTPATH
