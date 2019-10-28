ee2f84c966a9f76b74660245918255c535f8a3f8
MikeAWilliams <mumakin@gmail.com>
2019-10-27 22:23:50 -0600

    cleanup and add benchmark results

--- parallel_max_element/main.cpp
+++ parallel_max_element/main.cpp
@@ -1,10 +1,10 @@
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
 #define CATCH_CONFIG_MAIN 
 #include "catch2/catch.hpp"
 
 #include <algorithm>
 #include <thread>
 #include <vector>
-#include <iostream>
 
 std::vector<double> GetTestData(const size_t elements, const double mean, const double standardDeviation)
 {
@@ -25,22 +25,22 @@ std::vector<double> GetTestData(const size_t elements, const double mean, const 
 template<typename iterator>
 struct max_block
 {
-    iterator operator()(iterator first, iterator last, iterator& result)
+    void operator()(iterator first, iterator last, iterator& result)
     {
         result = std::max_element(first, last);
     }
 };
 
-int ComputeMaxThreads(const unsigned long length)
+size_t ComputeMaxThreads(const unsigned long length)
 {
-    const int min_per_thread = 25;
-    const int max_threads = (length + min_per_thread - 1) / min_per_thread;
+    const size_t min_per_thread = 25;
+    const size_t max_threads = (length + min_per_thread - 1) / min_per_thread;
 
-    const int hardware_threads = std::thread::hardware_concurrency();
+    const size_t hardware_threads = std::thread::hardware_concurrency();
 
     if(0 == hardware_threads)
     {
-        return std::min(2, max_threads);
+        return std::min(static_cast<size_t>(2), max_threads);
     }
     return std::min(hardware_threads, max_threads);
 }
@@ -52,26 +52,23 @@ int ComputeMaxThreads(const unsigned long length)
 template<typename iterator>
 iterator ParallelMaxElement(iterator begin, iterator end)
 {
-    const int length = std::distance(begin, end);
-
+    const size_t length = std::distance(begin, end);
     if(0 == length)
     {
         return begin;
     }
 
     const auto num_threads {ComputeMaxThreads(length)};
-    const int block_size = length / num_threads;
+    const size_t block_size {length / num_threads};
 
-    std::vector<iterator> results(num_threads);
-    std::vector<std::thread>  threads(num_threads - 1);
-
-    iterator block_start = begin;
-    for(unsigned long i=0; i < (num_threads-1); ++i)
+    std::vector<iterator> results{num_threads};
+    std::vector<std::thread>  threads{num_threads - 1};
+    iterator block_start {begin};
+    for(size_t i{0}; i < (num_threads-1); ++i)
     {
-        iterator block_end = block_start;
+        iterator block_end {block_start};
         std::advance(block_end, block_size);
-        threads[i] = std::thread(
-            max_block<iterator>(), block_start, block_end, std::ref(results[i]));
+        threads[i] = std::thread( max_block<iterator>(), block_start, block_end, std::ref(results[i]));
         block_start = block_end;
     }
     max_block<iterator>()(block_start, end, results[num_threads-1]);
@@ -90,4 +87,18 @@ TEST_CASE("Parallel max element")
 {
     const auto input {GetTestData(100'000, 500, 200)};
     REQUIRE(*std::max_element(input.cbegin(), input.cend()) == *ParallelMaxElement(input.cbegin(), input.cend()));
+}
+
+TEST_CASE("benchmarks")
+{
+    const auto input {GetTestData(10'000'000, 500, 200)};
+    BENCHMARK("Serial")
+    {
+        std::max_element(input.cbegin(), input.cend());
+    };
+
+    BENCHMARK("ParallelMaxElement")
+    {
+        ParallelMaxElement(input.cbegin(), input.cend());
+    };
 }
