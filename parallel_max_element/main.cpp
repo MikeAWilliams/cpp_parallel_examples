c52253ea4a0e5af29a5f1b20c46d6af788a5661d
MikeAWilliams <mumakin@gmail.com>
2019-10-27 21:15:17 -0600

    working solution

--- parallel_max_element/main.cpp
+++ parallel_max_element/main.cpp
@@ -2,7 +2,9 @@
 #include "catch2/catch.hpp"
 
 #include <algorithm>
+#include <thread>
 #include <vector>
+#include <iostream>
 
 std::vector<double> GetTestData(const size_t elements, const double mean, const double standardDeviation)
 {
@@ -18,18 +20,82 @@ std::vector<double> GetTestData(const size_t elements, const double mean, const 
     }
     return result;
 }
+// based on Anthony Williams parallel_accumulate
+template<typename iterator>
+struct max_block
+{
+    iterator operator()(iterator first, iterator last, iterator& result)
+    {
+        result = std::max_element(first, last);
+    }
+};
+
+int ComputeMaxThreads(const unsigned long length)
+{
+    const int min_per_thread = 25;
+    const int max_threads = (length + min_per_thread - 1) / min_per_thread;
+
+    const int hardware_threads = std::thread::hardware_concurrency();
+
+    if(0 == hardware_threads)
+    {
+        return std::min(2, max_threads);
+    }
+    return std::min(hardware_threads, max_threads);
+}
+
 // TODO after making this work as is, add overloads.
 // take a comparison function
 // take a number of threads allowed
 // take a thread provider or manager
 template<typename iterator>
-iterator ParalellMaxElement(iterator begin, iterator end)
+iterator ParallelMaxElement(iterator begin, iterator end)
 {
-    return begin;
+    const int length = std::distance(begin, end);
+
+    if(0 == length)
+    {
+        return begin;
+    }
+
+    const auto num_threads {ComputeMaxThreads(length)};
+    const int block_size = length / num_threads;
+
+    std::vector<iterator> results(num_threads);
+    std::vector<std::thread>  threads(num_threads - 1);
+
+    iterator block_start = begin;
+    for(unsigned long i=0;i<(num_threads-1);++i)
+    {
+        iterator block_end = block_start;
+        std::advance(block_end, block_size);
+        threads[i] = std::thread(
+            max_block<iterator>(), block_start, block_end, std::ref(results[i]));
+        block_start = block_end;
+    }
+    max_block<iterator>()(block_start, end, results[num_threads-1]);
+    
+    std::for_each(threads.begin(), threads.end(), std::mem_fn(&std::thread::join));
+
+    iterator result {results[0]};
+    for(iterator iter : results)
+    {
+        if(*iter > *result)
+        {
+            result = iter;
+        }
+    }
+    return result;
+    //return *std::max_element(results.begin(), results.end(), 
+        //[](iterator a, iterator b)
+        //{
+            //return *b < *a;
+        //});
 }
 
-TEST_CASE("Happy path")
+
+TEST_CASE("Parallel max element")
 {
     const auto input {GetTestData(100'000, 500, 200)};
-    REQUIRE(*std::max_element(input.cbegin(), input.cend()) == *ParalellMaxElement(input.cbegin(), input.cend()));
+    REQUIRE(*std::max_element(input.cbegin(), input.cend()) == *ParallelMaxElement(input.cbegin(), input.cend()));
 }
